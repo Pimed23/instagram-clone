3cb1f68d3236119bb713bf640fb279c405fbbff5
Gabriel Gonza <gabrielito.xvr@gmail.com>
2023-03-30 10:27:36 -0500

    wip: main feed

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import Header from "../elements/Header";
 import Footer from "../elements/Footer";
+import MainFeed from "../views/MainFeed";
 
 export default function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-zinc-200">
-      <div className="flex flex-col w-[600px] min-h-screen bg-amber-300">
+    <div className="flex justify-center items-center min-h-screen max-h-screen bg-zinc-200">
+      <div className="flex flex-col w-[600px] min-h-screen max-h-screen bg-amber-300">
         <Header />
-        <div className="flex-grow" />
+        <MainFeed />
         <Footer />
       </div>
     </div>
--- src/components/elements/PostCard.tsx
+++ src/components/elements/PostCard.tsx
@@ -1,5 +1,25 @@
-const PostCard = () => {
-  return <p>Hello World!</p>;
+import React, { FC, ReactElement } from "react";
+
+export type Post = {
+  title: string;
+  imageSrc: string;
+  from: string;
+  time: Date;
+};
+
+const PostCard: FC<Post> = ({ title, imageSrc, from, time }): ReactElement => {
+  return (
+    <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden flex-grow">
+      <div className="p-6">
+        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+        <p className="mt-2 text-gray-600">{from}</p>
+      </div>
+      <div
+        className="h-64 bg-cover bg-center"
+        style={{ backgroundImage: `url(${imageSrc})` }}
+      ></div>
+    </div>
+  );
 };
 
 export default PostCard;
